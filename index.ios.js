7e241ceb0032cad26c1b362dfd44836b446100f4
John Behan <john@sportdec.com>
2017-10-05 18:08:44 +0100

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+
+import Header from './src/components/header';
+
+const App = () => <Header text='Albums!'/>;
+
+AppRegistry.registerComponent('albums', () => App);
